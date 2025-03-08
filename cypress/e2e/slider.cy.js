cc93ac5b3d4d9113ed8615a37f7b5cabd1879699
krystiankey <kluczkiewicz1512@wp.pl>
2025-03-08 15:49:56 +0100

    Cypress test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,10 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if first slide contains "Rome"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
